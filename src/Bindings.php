4adbe66f790838ee43f8359dab4cec2874296679
James Ainooson <jainooson@gmail.com>
2017-12-19 00:35:43 -0600

    Updates and Docs - Updated dependencies - Wrote docs on bindings class

--- src/Bindings.php
+++ src/Bindings.php
@@ -10,32 +10,64 @@ class Bindings
 
     /**
      * An array of all the bindings.
+     *
      * @var array
      */
     private $bindings = [];
     
     /**
-     * An active key that would be altered with subsequent calls to the bindings object.
+     * Key of the binding that would be altered with subsequent calls to a bindings object.
+     *
      * @var string 
      */
     private $activeKey;
 
-    public function setActiveKey($activeKey)
+    /**
+     * Set the key of the binding to be modified or accessed on subsequent calls.
+     *
+     * @param $activeKey
+     * @return self
+     */
+    public function setActiveKey(string $activeKey) : self
     {
         $this->activeKey = $activeKey;
         return $this;
     }
 
-    public function call($method, $parameters = [])
+    /**
+     * Registers a method to be called when an instance is created for the current active binding.
+     * This method could be a setter or some other optional initialization method. Parameters for methods registered
+     * with call that have type hints are also injected.
+     *
+     * @param string $method
+     * @param array $parameters
+     * @return self
+     */
+    public function call(string $method, array $parameters = []) : self
     {
         $this->bindings[$this->activeKey]['calls'][] = [$method, $parameters];
+        return $this;
     }
 
-    public function setProperty($property, $binding)
+    /**
+     * Registers a property to be assigned a value when an instance is created for the current active binding.
+     *
+     * @param string $property
+     * @param string $value
+     * @return self
+     */
+    public function setProperty(string $property, string $value)
     {
-        $this->bindings[$this->activeKey]['sets'][$property] = $binding;
+        $this->bindings[$this->activeKey]['sets'][$property] = $value;
+        return $this;
     }
 
+    /**
+     * Provides a concrete class or factory function to which the currently selected binding should be linked.
+     *
+     * @param mixed $value
+     * @return self
+     */
     public function to($value)
     {
         if(isset($this->bindings[$this->activeKey])) {
@@ -46,22 +78,44 @@ class Bindings
         return $this;
     }
 
-    public function get($key)
+    /**
+     * Get the configuration of a binding.
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public function get(string $key)
     {
         return $this->bindings[$key];
     }
 
+    /**
+     * Checks if a binding exists.
+     *
+     * @param string $key
+     * @return bool
+     */
     public function has($key)
     {
         return isset($this->bindings[$key]);
     }
 
+    /**
+     * Register the current active binding as a singleton.
+     *
+     * @param bool $singleton
+     */
     public function asSingleton($singleton = true)
     {
         $this->bindings[$this->activeKey]['singleton'] = $singleton;
-    }    
-    
-    private function setArrayBinding($binding)
+    }
+
+    /**
+     * Setup bindings from an array.
+     *
+     * @param array $binding
+     */
+    private function setArrayBinding(array $binding)
     {
         if(isset($binding[0])) {
             $this->to($binding[0]);
@@ -94,7 +148,12 @@ class Bindings
         $this->asSingleton($binding['singleton'] ?? false);
     }
 
-    public function merge($bindings)
+    /**
+     * Merge current bindings with another configuration overwriting conflicting values with the new bindings.
+     *
+     * @param array $bindings
+     */
+    public function merge(array $bindings)
     {
         foreach($bindings as $key => $binding) {
             $this->activeKey = $key;
